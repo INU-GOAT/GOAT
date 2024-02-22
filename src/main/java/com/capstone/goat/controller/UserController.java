b6e829fc0adfdce075ab9858c260e3cfb3ed4456
hen715 <hen715@naver.com>
2024-02-22 23:37:38 +0900

    [Feat] mysql 연동 추가

--- src/main/java/com/capstone/goat/controller/UserController.java
+++ src/main/java/com/capstone/goat/controller/UserController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,6 +18,7 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users/")
+@CrossOrigin(origins = "*")
 public class UserController {
         private final UserService userService;
     @ApiOperation(value = "회원가입", notes = "바디에 {name,phone,loing_id,password,age,soccer_career,basketball_career,badminton_career,soccer_position,basketball_position}을 json형식으로 보내주세요. 경력이나 포지션을 입력하지 않았을경우 값에 -1을 담아 보내주세요")
