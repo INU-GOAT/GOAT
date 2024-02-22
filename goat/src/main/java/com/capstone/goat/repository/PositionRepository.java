1b43beab8e81d6193eb257adc437ab1759d89dd2
hen715 <hen715@naver.com>
2024-02-22 18:40:04 +0900

    파일 경로 변경

--- goat/src/main/java/com/capstone/goat/repository/PositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.goat.repository;
-
-import com.capstone.goat.domain.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position,Long> {
-}
