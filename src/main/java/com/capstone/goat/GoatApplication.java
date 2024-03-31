1c6f2e9437ff47ec2858a5912e6dbe2ff39f5d5c
hen715 <hen715@naver.com>
2024-04-01 03:05:26 +0900

    [Refactor] 서버 주소 변경

--- src/main/java/com/capstone/goat/GoatApplication.java
+++ src/main/java/com/capstone/goat/GoatApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition(
 		servers = {
-				@Server(url = "http://13.209.68.164/", description = "Server URL"),
+				@Server(url = "http://15.165.113.9/", description = "Server URL"),
 				@Server(url = "http://localhost:8080",description = "Local URL")
 		},
 		security = {
