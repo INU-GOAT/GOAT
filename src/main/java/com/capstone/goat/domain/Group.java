6b2fa1c8ecfc435e1af56527867e35f486f249a3
ImKEISS <kss5122@naver.com>
2024-04-04 18:27:33 +0900

    [Feat] 그룹장 컬럼 추가

--- src/main/java/com/capstone/goat/domain/Group.java
+++ src/main/java/com/capstone/goat/domain/Group.java
@@ -1,11 +1,15 @@
 package com.capstone.goat.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "group_table")
 public class Group {
@@ -14,6 +18,15 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "master_id")
+    User master;
+
     @OneToMany(mappedBy = "group",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     List<User> users;
+
+    @Builder
+    public Group(User master) {
+        this.master = master;
+    }
 }
