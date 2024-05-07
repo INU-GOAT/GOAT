e2a2fbdd5c2700144807655ab4a658e8b25fbcca
hen715 <hen715@naver.com>
2024-05-07 11:39:27 +0900

    [Feat] stomp 테스트 추가

--- src/main/java/com/capstone/goat/controller/ChatController.java
+++ src/main/java/com/capstone/goat/controller/ChatController.java
@@ -1,17 +1,24 @@
 package com.capstone.goat.controller;
 
+import com.capstone.goat.domain.User;
+import com.capstone.goat.dto.request.ChatDto;
 import com.capstone.goat.dto.response.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/chat/")
+@RequestMapping("/api/chat")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class ChatController {
+    private final SimpMessagingTemplate template;
     /*@Operation(summary = "메시지 전송", description = "사용자가 작성한 메시지를 전송합니다. url 바디에 {comment,time,gameId}를 담아 json 형태로 보내주세요.")
     @PostMapping()
     public ResponseEntity<?> chatAdd(){
@@ -23,4 +30,15 @@ public class ChatController {
     public ResponseEntity<?> chatList(Integer gameId){
         return new ResponseEntity<>(new ResponseDto(null,"성공"), HttpStatus.OK);
     }*/
+
+    @MessageMapping("/enter/{roomId}")
+    public void enter(@DestinationVariable Long roomId, ChatDto chatDto, @AuthenticationPrincipal User user){
+        template.convertAndSend("/room/chat/"+roomId,user.getNickname()+"님이 입장하셨습니다.");
+    }
+
+    @MessageMapping("/message/{roomId}")
+    public void message(@DestinationVariable Long roomId, ChatDto chatDto, @AuthenticationPrincipal User user){
+        template.convertAndSend("/room/chat/"+roomId,"메세지 테스트");
+    }
+
 }
---
+++ src/main/java/com/capstone/goat/dto/request/ChatDto.java
@@ -0,0 +1,12 @@
+package com.capstone.goat.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatDto {
+    String comment;
+}
